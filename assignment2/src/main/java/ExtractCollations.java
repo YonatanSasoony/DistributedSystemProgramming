c9a8b52b9de9357bc61e402a3167338fd86d1ffe
YonatanSasoony <ysasony@gmail.com>
2021-06-01 11:09:11 +0300

    init

---
+++ assignment2/src/main/java/ExtractCollations.java
@@ -0,0 +1,81 @@
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.*;
+import com.amazonaws.services.elasticmapreduce.util.StepFactory;
+import java.util.ArrayList;
+import java.util.List;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.Application;
+import com.amazonaws.services.elasticmapreduce.model.ComputeLimits;
+import com.amazonaws.services.elasticmapreduce.model.ComputeLimitsUnitType;
+import com.amazonaws.services.elasticmapreduce.model.InstanceGroupConfig;
+import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
+import com.amazonaws.services.elasticmapreduce.model.ManagedScalingPolicy;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowResult;
+import software.amazon.awssdk.services.ec2.model.*;
+import software.amazon.awssdk.services.ec2.model.Tag;
+
+
+public class ExtractCollations {
+    public static void main(String[] args) {
+
+        //Notice that order of commands matters.
+//        AWSCredentials credentials = new PropertiesCredentials(...);
+//        AmazonElasticMapReduce mapReduce = new
+//                AmazonElasticMapReduceClientBuilder.defaultClient(;
+
+
+        AWSCredentials credentials_profile = null;
+        try {
+            credentials_profile = new ProfileCredentialsProvider("default").getCredentials(); // specifies any named profile in .aws/credentials as the credentials provider
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load credentials from .aws/credentials file. " +
+                            "Make sure that the credentials file exists and that the profile name is defined within it.",
+                    e);
+        }
+
+        // create an EMR client using the credentials and region specified in order to create the cluster
+        AmazonElasticMapReduce mapReduce = AmazonElasticMapReduceClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials_profile))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+
+
+        HadoopJarStepConfig hadoopJarStep = new HadoopJarStepConfig()
+                .withJar("s3n://yourbucket/yourfile.jar") // This should be a full map reduce application.
+        .withMainClass("some.pack.MainClass")
+                .withArgs("s3n://yourbucket/input/", "s3n://yourbucket/output/");
+        StepConfig stepConfig = new StepConfig()
+                .withName("stepname")
+                .withHadoopJarStep(hadoopJarStep)
+                .withActionOnFailure("TERMINATE_JOB_FLOW");
+        JobFlowInstancesConfig instances = new JobFlowInstancesConfig()
+                .withInstanceCount(2)
+                .withMasterInstanceType(InstanceType.M4_LARGE.toString())
+                .withSlaveInstanceType(InstanceType.M4_LARGE.toString())
+                .withHadoopVersion("2.6.0").withEc2KeyName("yourkey")
+                .withKeepJobFlowAliveWhenNoSteps(false)
+                .withPlacement(new PlacementType("us-east-1a"));
+        RunJobFlowRequest runFlowRequest = new RunJobFlowRequest()
+                .withName("jobname")
+                .withInstances(instances)
+                .withSteps(stepConfig)
+                .withLogUri("s3n://yourbucket/logs/");
+        RunJobFlowResult runJobFlowResult = mapReduce.runJobFlow(runFlowRequest);
+        String jobFlowId = runJobFlowResult.getJobFlowId();
+        System.out.println("Ran job flow with id: " + jobFlowId);
+
+    }
+}
