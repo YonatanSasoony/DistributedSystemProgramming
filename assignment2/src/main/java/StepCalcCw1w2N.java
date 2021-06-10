8923822e5cb3487dd8578ad3d51c012cebb88374
Yossy <yossycarmeli@gmail.com>
2021-06-10 23:40:37 +0300

    Assignment2: divide to steps

--- assignment2/src/main/java/StepCalcCw1w2N.java
+++ assignment2/src/main/java/StepCalcCw1w2N.java
@@ -1,9 +1,16 @@
 import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 public class StepCalcCw1w2N {
@@ -21,41 +28,71 @@ public class StepCalcCw1w2N {
             String[] words = bigram.split(Defs.internalBigramDelimiter);
             if (words.length == 2 && !stopWords.contains(words)) {
                 context.write(decadeAndBigram, new LongWritable(occurrences));
+                context.write(new Text(decade), new LongWritable(occurrences));
             }
         }
     }
 
-    public static class ReducerClass extends Reducer<Text,LongWritable,Text,LongWritable> {
-        private long N;
-        private String prevDecade;
+    public static class CombinerClass extends Reducer<Text,LongWritable,Text,LongWritable> {
 
         @Override
-        protected void setup(Context context) {
-            N = 0;
-            prevDecade = "-1";
-        }
-
-        @Override
-        public void reduce(Text decadeAndBigram, Iterable<LongWritable> occurrences , Context context) throws IOException,  InterruptedException {
-            String decade = decadeAndBigram.toString().split(Defs.decadeBigramDelimiter)[0];
-            long totalDecadeOccurrences = occurrences.iterator().next().get();
-
-            context.write(decadeAndBigram, new LongWritable(totalDecadeOccurrences));
-            if (prevDecade.equals(decade)) {
-                N += totalDecadeOccurrences;
-            } else { // write N
-                prevDecade = decade;
-                context.write(new Text(decade+Defs.NTag), new LongWritable(N));
-                N = totalDecadeOccurrences;
+        // <decade#bigram, {occ}>, <decade, {occ1, .. occK}>
+        public void reduce(Text key, Iterable<LongWritable> occurrences, Context context) throws IOException, InterruptedException {
+            long totalOccurrences = 0;
+            for (LongWritable occurrence: occurrences) {
+                totalOccurrences += occurrence.get();
             }
+            context.write(key, new LongWritable(totalOccurrences));
         }
     }
 
     public static class PartitionerClass extends Partitioner<Text, LongWritable> {
         @Override
-        public int getPartition(Text decadeAndBigram, LongWritable value, int numPartitions) {
-            String decade = decadeAndBigram.toString().split(Defs.decadeBigramDelimiter)[0];
-            return decade.hashCode() % numPartitions;
+        // <decade#bigram, {occ}>, <decade, {occ1, .. occK}>
+        public int getPartition(Text key, LongWritable value, int numPartitions) {
+            return key.hashCode() % numPartitions;
         }
+    }
+
+    public static class ReducerClass extends Reducer<Text,LongWritable,Text,LongWritable> {
+        @Override
+        // <decade#bigram, {occ}>, <decade, {occ1, .. occK}>
+        public void reduce(Text key, Iterable<LongWritable> occurrences , Context context) throws IOException,  InterruptedException {
+            long totalOccurrences = 0;
+            for (LongWritable occurrence: occurrences) {
+                totalOccurrences += occurrence.get();
+            }
+
+            if (key.toString().contains(Defs.decadeBigramDelimiter)) { // <decade#bigram, {occ}
+                context.write(key, new LongWritable(totalOccurrences));
+            } else { // <decade, {occ1, .. occK}>
+                context.write(new Text(key +Defs.NTag), new LongWritable(totalOccurrences));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String jarName = args[0];
+        String input = args[1];
+        String output = args[2];
+        System.out.println("Hello "+jarName+" main");
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, jarName);
+        job.setJarByClass(StepCalcCw1w2N.class);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setMapperClass(MapperClass.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        job.setCombinerClass(CombinerClass.class);
+        job.setPartitionerClass(PartitionerClass.class);
+        job.setReducerClass(ReducerClass.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
